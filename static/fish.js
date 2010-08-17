b867ea8c03853db18019fa5c557b4652ae22765c
Hunter Loftis <hunter@skookum.com>
2010-08-16 21:39:23 -0400

    Fish generation going great, lots of lovely fish with some subtle effects.

--- static/fish.js
+++ static/fish.js
@@ -42,11 +42,22 @@ Fish.prototype._add_fin = function(c, ctx, w) {
 		console.log("f(" + k + ") = " + f[k]);
 	}
 	
-	ctx.fillStyle = 'rgba(255, 255, 255, .3)';
+	var grad = ctx.createLinearGradient(0.5 * c.width, 0, 0.5 * c.width, c.height);
+	/*
+	grad.addColorStop(0, '#ffcc00');
+	grad.addColorStop(.25, '#e69800');
+	grad.addColorStop(.8, '#a25923')
+	*/
+	grad.addColorStop(0, 'rgba(255,255,255,.25)');
+	grad.addColorStop(.5, 'rgba(255,255,255,.1)');
+	grad.addColorStop(1, 'rgba(255,255,255,.5)');
+	ctx.fillStyle = grad;
+	//ctx.fillStyle = 'rgba(255, 255, 255, .3)';
 	ctx.beginPath();
 	ctx.moveTo(f.s, c.height * .5);
-	ctx.lineTo(
-		f.s - f.l, f.y
+	ctx.quadraticCurveTo(
+		f.s - f.l * .5, f.y,
+		f.s - f.l * 1.35, f.y
 	);
 	ctx.lineTo(
 		f.s - f.l, c.height * .5
@@ -58,13 +69,13 @@ Fish.prototype.prepare = function() {
 	var c = this.canvas,
 		ctx = c.getContext('2d');
 
-	// Draw the body
+	// Compute the body
 	
 	var b = {		
 		fb: c.width * rand(.3, .75),		// Front body bulge X
-		rb: c.width * rand(.2, .25),		// Rear body bulge X
+		rb: c.width * rand(.15, .25),		// Rear body bulge X
 		f: c.height * rand(0, 0.35),			// Fat Y
-		pt: c.height * rand(0.4, .8),		// Tail point distance
+		pt: c.height * rand(0.25, .75),		// Tail point distance
 		tc: c.height * rand(0, 0.4),			// Tail cut center size		
 		td: c.width * rand(0, .2)			// Tail depth		
 	};
@@ -74,10 +85,18 @@ Fish.prototype.prepare = function() {
 		this._add_fin(c, ctx, c.width - b.rb);
 	}
 	
+	// Draw the body
+
+
 	var grad = ctx.createLinearGradient(0.5 * c.width, 0, 0.5 * c.width, c.height);
+	/*
 	grad.addColorStop(0, '#ffcc00');
 	grad.addColorStop(.25, '#e69800');
 	grad.addColorStop(.8, '#a25923')
+	*/
+	grad.addColorStop(0, '#fff');
+	grad.addColorStop(.25, '#bbb');
+	grad.addColorStop(.8, '#333');
 	
 	
 	ctx.fillStyle = grad;
@@ -85,26 +104,30 @@ Fish.prototype.prepare = function() {
 	ctx.moveTo(c.width, c.height * .5);		// Nose
 	ctx.bezierCurveTo(						// Nose -> Top tail point
 		b.fb, c.height + b.f,
-		b.rb, 0,
-		0, b.pt
+		b.rb, c.height * .15,
+		0, b.pt				// Top tail point
 	);
 	ctx.bezierCurveTo(						// Top tail point -> Bottom tail point
 		b.td, c.height * .5 - b.tc,
 		b.td, c.height * .5 + b.tc,
-		0, c.height - b.pt
+		0, c.height - b.pt				// Bottom tail point
 	);
 	ctx.bezierCurveTo(						// Bottom tail point -> Nose
-		b.rb, c.height,
+		b.rb, c.height - c.height * .15,
 		b.fb, -b.f,
 		c.width, c.height * .5
 	);
+
+
 	ctx.fill();
+	
+	//ctx.shadowColor = 'transparent black';
 	
 	// Draw the eye
 	
-	var eye_h = c.height * rand(0, .2),				// Eye height
-		eye_s = c.height * rand(.05, .15), 			// Eye size
-		pupil_s = eye_s * rand(.2, .9),				// Pupil size
+	var eye_h = c.height * rand(0, .15),				// Eye height
+		eye_s = c.height * rand(.05, .10), 			// Eye size
+		pupil_s = eye_s * rand(.2, .8),				// Pupil size
 		pupil_o = (eye_s - pupil_s) * rand(.2, .5),	// Pupil offset from center
 		pupil_a = rand(0, Math.PI * 2);				// Pupil angle
 	
@@ -115,7 +138,10 @@ Fish.prototype.prepare = function() {
 		eye_s, 0, Math.PI * 2,
 		false
 	);
+	
+	glow(ctx, 3, 'rgba(0,0,0,0.25)');		
 	ctx.fill();
+	glow(ctx, 0, '');
 	
 	ctx.fillStyle = '#000000';
 	ctx.beginPath();
@@ -130,6 +156,9 @@ Fish.prototype.prepare = function() {
 Fish.prototype.render = function(ctx) {
 	var c = this.canvas;
 	console.log("Rendering at " + this.x + ", " + this.y + " dims: " + c.width + "x" + c.height);
+
+	glow(ctx, 15, 'rgba(255,255,255,0.15)');
+	
 	ctx.drawImage(c, this.x, this.y, c.width, c.height);
 };
 
@@ -143,12 +172,24 @@ function rand(a, b) {
 	return Math.random() * (b - a) + a;
 }
 
+// When precomputing, seems to lock transparency so you can't do this in the original sprites =(
+function glow(ctx, dist, color) {
+	ctx.shadowOffsetX = 0;
+	ctx.shadowOffsetY = 0;
+	ctx.shadowBlur = dist;
+	ctx.shadowColor = color;
+}
+
 var view = new View('view');
-var sea = new Sea(800, 480);
-var fish = new Fish(100, 100, 100, 100);
+var sea = new Sea(view.canvas.width, view.canvas.height);
 
 sea.prepare();
-fish.prepare();
-
 sea.render(view.ctx);
-fish.render(view.ctx);
+
+for (var x = 0; x < 5; x++) {
+	for (var y = 0; y < 3; y++) {
+		var fish = new Fish(50 + 200 * x, 50 + 150 * y, 100, 100);
+		fish.prepare();
+		fish.render(view.ctx);
+	}
+}
