bc5b92ca8b9987d2d3f5bac5c4289e81233470e8
Hunter Loftis <hunter@skookum.com>
2010-08-17 17:57:28 -0400

    Optimized refresh, made full window, very sexy, need to figure out how to do background. Maybe as simple as two canvases, one with an aquarium and one with fish.

--- static/hunter.js
+++ static/hunter.js
@@ -1,3 +1,22 @@
+// !View
+
+function View(el_id) {
+	this.canvas = document.getElementById(el_id);
+	this.ctx = this.canvas.getContext('2d');
+	
+	var canvas = document.getElementById('view');
+	canvas.width = window.innerWith || document.documentElement.clientWidth;
+	canvas.height = window.innerHeight || document.documentElement.clientHeight;
+}
+View.prototype.resize = function(w, h) {
+	this.canvas.width = w;
+	this.canvas.height = h;
+};
+View.prototype.clear = function() {
+	this.canvas.width = this.canvas.width;
+}
+
+
 // !Sea
 
 function Sea(w, h) {
@@ -279,10 +298,6 @@ Fish.prototype.render = function(ctx, frame) {
 
 // !Utilities
 
-function View(el_id) {
-	this.canvas = document.getElementById(el_id);
-	this.ctx = this.canvas.getContext('2d');
-}
 
 function rand(a, b) {
 	return Math.random() * (b - a) + a;
