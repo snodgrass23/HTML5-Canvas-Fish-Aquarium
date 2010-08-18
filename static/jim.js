ea3620bff324ea39b4513bfcf658810566e9ce19
unknown <Jim@.(none)>
2010-08-18 09:06:19 -0400

    aquarium water

--- static/jim.js
+++ static/jim.js
@@ -7,6 +7,7 @@ Aquarium.prototype.prepare = function () {
 	var c = this.canvas,
 		ctx = c.getContext('2d');
 	
+    // stroke for main frame
 	ctx.strokeStyle = "#666";
     ctx.lineWidth = 10;
     var topLine = ctx.lineWidth/2;
@@ -15,11 +16,12 @@ Aquarium.prototype.prepare = function () {
     ctx.lineTo(c.width-ctx.lineWidth/2,topLine); 
     ctx.lineTo(c.width-ctx.lineWidth/2,c.height-ctx.lineWidth/2); 
     ctx.lineTo(ctx.lineWidth/2,c.height-ctx.lineWidth/2);
-    ctx.lineTo(ctx.lineWidth/2,topLine);
+    ctx.lineTo(ctx.lineWidth/2,topLine-ctx.lineWidth);
     ctx.stroke();
     
 	ctx.globalCompositeOperation = "source-atop";
     
+    // shading for main frame
 	var grad = ctx.createLinearGradient(0.5 * c.width, 0, 0.5 * c.width, c.height);
 	grad.addColorStop(0, 'rgba(255,255,255,.0)');
 	grad.addColorStop(.2, 'rgba(255,255,255,.2)');
@@ -34,15 +36,32 @@ Aquarium.prototype.prepare = function () {
     
     ctx.globalAlpha = .3;
     
+    // dimensions of 3d tank
     ctx.strokeStyle = "#333";
     ctx.lineWidth = 4;
     var border = 75;
     ctx.beginPath();
-    ctx.moveTo(border,border); 
-    ctx.lineTo((c.width-ctx.lineWidth/2) - border,border); 
+    
+    //  back of 3d tank
+    ctx.moveTo(border,border - 20); 
+    ctx.lineTo((c.width-ctx.lineWidth/2) - border,border - 20); 
     ctx.lineTo((c.width-ctx.lineWidth/2) - border,(c.height-ctx.lineWidth/2) - border); 
     ctx.lineTo(border,(c.height-ctx.lineWidth/2) - border);
-    ctx.lineTo(border,border);
+    ctx.lineTo(border,border-ctx.lineWidth/2 - 20);
+    
+    // diagonals of 3d tank
+    //top left corners
+    ctx.moveTo(border,border - 20);
+    ctx.lineTo(0,0);
+    //top right corners
+    ctx.moveTo((c.width-ctx.lineWidth/2) - border,border - 20);
+    ctx.lineTo(c.width,0);
+    //bottom right corners
+    ctx.moveTo((c.width-ctx.lineWidth/2) - border,(c.height-ctx.lineWidth/2) - border);
+    ctx.lineTo(c.width,c.height);
+    //bottom left corners
+    ctx.moveTo(border,(c.height-ctx.lineWidth/2) - border);
+    ctx.lineTo(0,c.height);
     ctx.stroke();
     
 	ctx.globalCompositeOperation = "source-atop";
@@ -52,15 +71,28 @@ Aquarium.prototype.prepare = function () {
     
     ctx.globalAlpha = .5;
     
+    // shadow on outside of frame
     ctx.strokeStyle = "#000";
     ctx.lineWidth = 2;
-    var topLine = ctx.lineWidth/2;
+    var topLine = ctx.lineWidth;
     ctx.beginPath();
     ctx.moveTo(0,topLine); 
-    ctx.lineTo(c.width-ctx.lineWidth/2,topLine); 
-    ctx.lineTo(c.width-ctx.lineWidth/2,c.height-ctx.lineWidth/2); 
-    ctx.lineTo(ctx.lineWidth/2,c.height-ctx.lineWidth/2);
-    ctx.lineTo(ctx.lineWidth/2,topLine);
+    ctx.lineTo(c.width-ctx.lineWidth,topLine); 
+    ctx.lineTo(c.width-ctx.lineWidth,c.height-ctx.lineWidth/2); 
+    ctx.lineTo(ctx.lineWidth,c.height-ctx.lineWidth/2);
+    ctx.lineTo(ctx.lineWidth,topLine);
+    ctx.stroke();
+    
+    // highlight on inside of frame
+    ctx.strokeStyle = "#FFF";
+    ctx.lineWidth = 2;
+    var offset = 10;
+    ctx.beginPath();
+    ctx.moveTo(offset,offset); 
+    ctx.lineTo(c.width - offset,offset); 
+    ctx.lineTo(c.width - offset,c.height - offset); 
+    ctx.lineTo(offset, c.height - offset);
+    ctx.lineTo(offset,offset-ctx.lineWidth/2);
     ctx.stroke();
 
 	
@@ -69,3 +101,59 @@ Aquarium.prototype.render = function(ctx) {
 	var c = this.canvas;
 	ctx.drawImage(c, 0, 0, c.width, c.height);
 };
+
+// Water
+
+function Water(w, h) {
+	this.canvas = document.createElement('canvas');
+	this.canvas.width = w;
+	this.canvas.height = h;
+}
+Water.prototype.prepare = function () {
+	var c = this.canvas,
+		ctx = c.getContext('2d');
+	
+    ctx.strokeStyle = "#7787f4";
+    ctx.lineWidth = 3;
+    var waveWidth = 75;
+    var waterHeight = 50;
+    for(var i = 0; i < c.width; i += waveWidth) {
+        ctx.moveTo(i, waterHeight);
+        ctx.bezierCurveTo(
+	    	i + 20, waterHeight + 15,
+	    	i + waveWidth - 20, waterHeight + 30,
+	    	i + waveWidth, waterHeight
+	    );
+    }
+    ctx.lineTo(c.width, c.height);
+    ctx.lineTo(0, c.height);
+    ctx.lineTo(0, waterHeight);
+    
+    ctx.stroke();
+    
+    var grad = ctx.createLinearGradient(0.5 * c.width, 0, 0.5 * c.width, c.height);
+	grad.addColorStop(0, '#78c9eb');
+	grad.addColorStop(.04, '#78c9eb');
+	grad.addColorStop(.5, '#b5e1f4');
+	grad.addColorStop(1, '#46b5e3');
+    
+    ctx.fillStyle = grad;
+    ctx.fill();
+    
+};
+Water.prototype.render = function(ctx) {
+	var c = this.canvas;
+	ctx.drawImage(c, 0, 0, c.width, c.height);
+};
+
+
+
+
+
+
+
+
+
+
+
+
